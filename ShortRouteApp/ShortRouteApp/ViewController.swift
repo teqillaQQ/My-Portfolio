cf0e3f23d6e01636af0871114b41d48494a50b3c
Alexander <polskibandit@gmail.com>
2022-01-04 14:02:09 +0300

    finish mapApp

--- ShortRouteApp/ShortRouteApp/ViewController.swift
+++ ShortRouteApp/ShortRouteApp/ViewController.swift
@@ -49,24 +49,108 @@ class ViewController: UIViewController {
         resetButton.translatesAutoresizingMaskIntoConstraints = false
         return resetButton
     }()
+    
+    var annotationsArray = [MKPointAnnotation]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         setConstrains()
         addAdressButton.addTarget(self, action: #selector(addAdressButtonTupped), for: .touchUpInside)
         routeButton.addTarget(self, action: #selector(routeButtonTupped), for: .touchUpInside)
         resetButton.addTarget(self, action: #selector(resetButtonTupped), for: .touchUpInside)
     }
     @objc func addAdressButtonTupped() {
-        alertAddAdress(title: "Добавить", placeholder: "введите адрес") { (text) in
-            print(text)
+        alertAddAdress(title: "Добавить", placeholder: "введите адрес") {[self] (text) in
+            satupPlacemark(adressPlace: text)
         }
     }
     @objc func routeButtonTupped() {
-        print("route")
+       
+        for index in 0...annotationsArray.count - 2 {
+            
+            createDirectionRequest(startCoordinate: annotationsArray[index].coordinate, destinationCoordinate: annotationsArray[index + 1].coordinate)
+        }
+        
+        mapView.showAnnotations(annotationsArray, animated: true)
     }
     @objc func resetButtonTupped() {
-        print("reset")
+        mapView.removeOverlays(mapView.overlays)
+        mapView.removeAnnotations(mapView.annotations)
+        annotationsArray = [MKPointAnnotation]()
+        routeButton.isHidden = true
+        resetButton.isHidden = true
+    }
+    
+    private func satupPlacemark(adressPlace: String) {
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(adressPlace) {[self] (placemarks, error) in
+            
+            if let error = error {
+                print(error)
+                alertError(title: "Ошибка", message: "Сервер не доступен. Попробуйте добавить адрес еще раз")
+                return
+            }
+            
+            guard let placemarks = placemarks else { return }
+            let placemark = placemarks.first
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = "\(adressPlace)"
+            guard let placemarkLocation = placemark?.location else { return }
+            annotation.coordinate = placemarkLocation.coordinate
+            
+            annotationsArray.append(annotation)
+            
+            if annotationsArray.count > 2 {
+                routeButton.isHidden = false
+                resetButton.isHidden = false
+            }
+            
+            mapView.showAnnotations(annotationsArray, animated: true)
+        }
+    }
+    
+    private func createDirectionRequest(startCoordinate: CLLocationCoordinate2D, destinationCoordinate: CLLocationCoordinate2D) {
+        let startLocation = MKPlacemark(coordinate: startCoordinate)
+        let destinationLocation = MKPlacemark(coordinate: destinationCoordinate)
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: startLocation)
+        request.destination = MKMapItem(placemark: destinationLocation)
+        request.transportType = .automobile
+        request.requestsAlternateRoutes = true
+        
+        let diraction = MKDirections(request: request)
+        diraction.calculate { (response, error) in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let response = response else {
+                self.alertError(title: "Ошибка", message: "Маршрут не доступен")
+                return
+            }
+            
+            var minRoute = response.routes[0]
+            for rout in response.routes {
+                minRoute = (rout.distance < minRoute.distance) ? rout : minRoute
+            }
+            
+            self.mapView.addOverlay(minRoute.polyline)
+        
+        }
+    }
+}
+extension ViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        let render = MKPolylineRenderer(overlay: overlay as! MKPolyline)
+        render.strokeColor = .red
+        return render
     }
 }
 
@@ -103,7 +187,6 @@ extension ViewController {
             resetButton.widthAnchor.constraint(equalToConstant: 110),
             resetButton.heightAnchor.constraint(equalToConstant: 70)
         ])
-
     }
 }
 
